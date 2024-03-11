e31a2c3a35055f42d933b67ef0aa5a1d61fced65
abel <abelkinfu678@gmail.com>
2024-03-12 01:25:27 +0300

    sth is changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -66,7 +66,6 @@ const Header = ({ toggleSideMenu }) => {
               ></path>
             </svg>
           </button>
-          {/* Search input */}
           <div className="flex justify-center flex-1 lg:mr-32">
             <div className="relative w-full max-w-xl mr-6 focus-within:text-purple-500">
               <div className="absolute inset-y-0 flex items-center pl-2">
@@ -84,7 +83,7 @@ const Header = ({ toggleSideMenu }) => {
                 </svg>
               </div>
               <input
-                className="w-full pl-8 pr-2 text-sm text-gray-700 placeholder-gray-600 bg-gray-100 border-0 rounded-md dark:placeholder-gray-500 dark:focus:shadow-outline-gray dark:focus:placeholder-gray-600 dark:bg-gray-700 dark:text-gray-200 focus:placeholder-gray-500 focus:bg-white focus:border-purple-300 focus:outline-none focus:shadow-outline-purple form-input"
+                className="w-full pl-8 py-2 outline-none pr-2 text-sm text-gray-700 placeholder-gray-600 bg-gray-100 border-0 rounded-md dark:placeholder-gray-500 dark:focus:shadow-outline-gray dark:focus:placeholder-gray-600 dark:bg-gray-700 dark:text-gray-200 focus:placeholder-gray-500 focus:bg-white focus:border-purple-300 focus:outline-none focus:shadow-outline-purple form-input"
                 type="text"
                 placeholder="Search for projects"
                 aria-label="Search"

d6e4d7ef428b5bf75455020e6b5ebd1e9e19410e
abel <abelkinfu678@gmail.com>
2024-03-21 15:32:29 +0300

    sth is updated

--- src/store.js
+++ src/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { productsApi } from "./features/manage-products/api/productsApi";
 import { ordersApi } from "./features/manage-orders/api/ordersApi";
+import { blogApi } from "./features/manage-blogs/api/blogApi";
 
 export const store = configureStore({
   reducer: {
     [productsApi.reducerPath]: productsApi.reducer,
     [ordersApi.reducerPath]: ordersApi.reducer,
+    [blogApi.reducerPath]: blogApi.reducer,
   },
 
   middleware: (getDefaultMiddleware) =>

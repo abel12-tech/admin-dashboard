3d8da0aa8c161c8cb80edb90d3370330a182a5e9
abel <abelkinfu678@gmail.com>
2024-04-01 16:21:09 +0300

    sidebar icons from svg to react-icons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useDarkMode } from "../shared/darkModeContext";
+import { MdOutlineDashboard } from "react-icons/md";
+import { RiProductHuntLine } from "react-icons/ri";
+import { MdOutlineCategory } from "react-icons/md";
+import { BsBorderStyle } from "react-icons/bs";
+import { GrBlog } from "react-icons/gr";
+import { BiCategoryAlt } from "react-icons/bi";
+import { MdPayment } from "react-icons/md";
+import { MdOutlineWarehouse } from "react-icons/md";
 
 const Sidebar = ({ isSideMenuOpen }) => {
   const [activeLink, setActiveLink] = useState("/");
@@ -42,21 +50,11 @@ const Sidebar = ({ isSideMenuOpen }) => {
               className={`inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 ${
                 isDarkMode
                   ? "text-gray-100 dark:hover:text-gray-200"
-                  : "text-gray-800 dark:hover:text-gray-200"
+                  : "text-gray-800 dark:hover:text-gray-600"
               }`}
             >
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
-              </svg>
+              <MdOutlineDashboard className="w-5 h-5" />
+
               <span className="ml-4">Dashboard</span>
             </Link>
           </li>
@@ -76,21 +74,11 @@ const Sidebar = ({ isSideMenuOpen }) => {
               className={`inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 ${
                 isDarkMode
                   ? "dark:hover:text-gray-200"
-                  : "dark:hover:text-gray-200"
+                  : "text-gray-800 dark:hover:text-gray-600"
               }`}
             >
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path>
-              </svg>
+              <RiProductHuntLine className="w-5 h-5" />
+
               <span className="ml-4">Manage Products</span>
             </Link>
           </li>
@@ -107,21 +95,11 @@ const Sidebar = ({ isSideMenuOpen }) => {
               className={`inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 ${
                 isDarkMode
                   ? "dark:hover:text-gray-200"
-                  : "dark:hover:text-gray-200"
+                  : "text-gray-800 dark:hover:text-gray-600"
               }`}
             >
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path>
-              </svg>
+              <MdOutlineCategory className="w-5 h-5" />
+
               <span className="ml-4">Manage Product Categories</span>
             </Link>
           </li>
@@ -138,21 +116,11 @@ const Sidebar = ({ isSideMenuOpen }) => {
               className={`inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 ${
                 isDarkMode
                   ? "dark:hover:text-gray-200"
-                  : "dark:hover:text-gray-200"
+                  : "text-gray-800 dark:hover:text-gray-600"
               }`}
             >
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"></path>
-              </svg>
+              <BsBorderStyle className="w-5 h-5" />
+
               <span className="ml-4">Manage Orders</span>
             </Link>
           </li>
@@ -169,21 +137,11 @@ const Sidebar = ({ isSideMenuOpen }) => {
               className={`inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 ${
                 isDarkMode
                   ? "dark:hover:text-gray-200"
-                  : "dark:hover:text-gray-200"
+                  : "text-gray-800 dark:hover:text-gray-600"
               }`}
             >
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path>
-              </svg>
+              <GrBlog className="w-5 h-5" />
+
               <span className="ml-4">Manage Blogs</span>
             </Link>
           </li>
@@ -200,21 +158,11 @@ const Sidebar = ({ isSideMenuOpen }) => {
               className={`inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 ${
                 isDarkMode
                   ? "dark:hover:text-gray-200"
-                  : "dark:hover:text-gray-200"
+                  : "text-gray-800 dark:hover:text-gray-600"
               }`}
             >
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"></path>
-              </svg>
+              <BiCategoryAlt className="w-5 h-5" />
+
               <span className="ml-4">Manage Blog Category</span>
             </Link>
           </li>
@@ -231,21 +179,11 @@ const Sidebar = ({ isSideMenuOpen }) => {
               className={`inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 ${
                 isDarkMode
                   ? "dark:hover:text-gray-200"
-                  : "dark:hover:text-gray-200"
+                  : "text-gray-800 dark:hover:text-gray-600"
               }`}
             >
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"></path>
-              </svg>
+              <MdPayment className="w-5 h-5" />
+
               <span className="ml-4">Manage Payment</span>
             </Link>
           </li>
@@ -262,21 +200,11 @@ const Sidebar = ({ isSideMenuOpen }) => {
               className={`inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 ${
                 isDarkMode
                   ? "dark:hover:text-gray-200"
-                  : "dark:hover:text-gray-200"
+                  : "text-gray-800 dark:hover:text-gray-600"
               }`}
             >
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"></path>
-              </svg>
+              <MdOutlineWarehouse className="w-5 h-5" />
+
               <span className="ml-4">Manage Warehouses</span>
             </Link>
           </li>

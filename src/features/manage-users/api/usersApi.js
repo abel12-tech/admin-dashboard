531eb26ce3f5ecce2dd5dbe01e958338681aa8df
abel <abelkinfu678@gmail.com>
2024-04-05 23:13:41 +0300

    ensuring the token is fetched correctly and then passed to the headers before API call is done

--- src/features/manage-users/api/usersApi.js
+++ src/features/manage-users/api/usersApi.js
@@ -2,20 +2,26 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { getTokenFromCookies } from "../../../shared/getToken.mjs";
 import { BASE_URL } from "../../../constants";
 
-
-
 export const usersApi = createApi({
   reducerPath: "usersApi",
-  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL,
+    prepareHeaders: async (headers) => {
+      // Fetch token asynchronously
+      const token = await getTokenFromCookies();
+      if (token) {
+        // If token exists, include it in the Authorization header
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
   endpoints: (builder) => ({
     getAllUsers: builder.query({
-      query: () => `/user`,
-      method:"GET",
-      headers: {
-        Authorization: `Bearer ${getTokenFromCookies()}`,
-      },
+      query: () => "/user",
+      method: "GET",
     }),
   }),
 });
 
-export const {useGetAllUsersQuery} = usersApi;
+export const { useGetAllUsersQuery } = usersApi;

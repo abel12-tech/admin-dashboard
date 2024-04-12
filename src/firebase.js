84cc97c161ecba858191d1ad1f887454b17f2dd3
Zyonatay Gashaw <147905444+Zygashaw@users.noreply.github.com>
2024-04-12 01:25:54 -0700

    firebase changed

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
+import { getStorage} from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBUs93x1c4-NyA9KggvCLO5qKu3-mwX2ao",
-  authDomain: "farmer-ecommerce-files.firebaseapp.com",
-  projectId: "farmer-ecommerce-files",
-  storageBucket: "farmer-ecommerce-files.appspot.com",
-  messagingSenderId: "716099671539",
-  appId: "1:716099671539:web:534a047434e79b2e6e6e86",
-  measurementId: "G-DV1ZX0H39M",
+  apiKey: "AIzaSyBnEq160JSzDQVWBIEx1wIFpISC6yyzg98",
+  authDomain: "ecommerce-d8dbb.firebaseapp.com",
+  projectId: "ecommerce-d8dbb",
+  storageBucket: "ecommerce-d8dbb.appspot.com",
+  messagingSenderId: "149182718584",
+  appId: "1:149182718584:web:b36badcf6c6cfae9c9c38d",
+  measurementId: "G-KVEN7S9F4C"
 };
 
 // Initialize Firebase
